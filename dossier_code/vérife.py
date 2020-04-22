4684a1e78179afd98f3bb4a80c813e8ac872bda0
Your Name <robertmaxence35@gmail.com>
2020-04-22 13:44:55 +0200

    1 ère version MAx

--- dossier_code/vérife.py
+++ dossier_code/vérife.py
@@ -18,5 +18,4 @@ def generate_random_points(n):
 random_points = generate_random_points(20)  # génére des points aléatoires
 cloud = PointCloud(random_points)           # crée le nuage de points
 cloud.draw()                                # ajoute le nuage de points au graphique
-#cloud.compute_convex_hull().draw()          # ajoute l'enveloppe convexe au graphiqye
 plt.show()
