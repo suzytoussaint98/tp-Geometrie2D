1ad16e82c4df13ec73a8d386f841d36518050408
Your Name <robertmaxence35@gmail.com>
2020-04-22 11:14:29 +0200

    1 ère version MAx

---
+++ dossier_code/Point.py
@@ -0,0 +1,15 @@
+class Point:
+
+    def __init__(self, x, y):
+        self.y = y
+        self.x = x
+
+    def equals(self, z):
+    if a.x == b.x and a.y == b.y
+    return true
+
+
+
+
+
+
---
+++ dossier_code/Programme.py
@@ -0,0 +1,6 @@
+from dossier_code.Point import Point
+
+a = Point(0, 1)
+b = Point(2,8)
+
+if a.x == b.x and a.y == b.y
